a237b836a165fb5aff9636019cd1f7212bd46736
Sivashankar <79368311+Siva-PythonPirates@users.noreply.github.com>
2023-09-01 00:03:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+var nameInput = document.getElementById('name')
+var downloadBtn = document.getElementById('download-btn')
+
+var image = new Image()
+image.crossOrigin="anonymous";
+image.src = 'cert.jpg'
+image.onload = function () {
+	drawImage()
+}
+
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '35px "Alex Brush"';
+	ctx.fillStyle = '#114d00';
+	ctx.fillText(nameInput.value, 300, 238)
+}
+
+nameInput.addEventListener('input', function () {
+	drawImage()
+})
+
+downloadBtn.addEventListener('click', function () {
+	downloadBtn.href = canvas.toDataURL('image/jpg')
+	downloadBtn.download = 'Certificate - ' + nameInput.value
+})
